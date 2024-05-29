1bf5363766f7538707fef215eac54a6a67b12491
Nearere <97075670+BeaverByte@users.noreply.github.com>
2024-05-28 19:16:04 -0500

    Add thead and tbody for table

--- src/Eligibility.tsx
+++ src/Eligibility.tsx
@@ -27,16 +27,20 @@ function Eligibility() {
         <h2>Riders</h2>
         <section>
           <table className="riders">
-            <tr>
-              <th>Header 1</th>
-            </tr>
-            <tr>
-              <td>Data 1</td>
-              <td>Data 2</td>
-              <td>Data 3</td>
-              <td>Data 4</td>
-              <td>Data 5</td>
-            </tr>
+            <thead>
+              <tr>
+                <th>Header 1</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Data 1</td>
+                <td>Data 2</td>
+                <td>Data 3</td>
+                <td>Data 4</td>
+                <td>Data 5</td>
+              </tr>
+            </tbody>
           </table>
         </section>
       </div>
